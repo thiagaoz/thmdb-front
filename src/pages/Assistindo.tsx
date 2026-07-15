d59059a27bd98daacb4431d2df5cf9a11580b88f
zeron <thiagaoz@proton.me>
2026-07-15 14:54:13 -0300

    fix: carregamento dos poster fix: posicao do navbar em todas as telas

--- src/pages/Assistindo.tsx
+++ src/pages/Assistindo.tsx
@@ -8,17 +8,21 @@ import type Seriados from './Seriados';
 import { Link } from 'react-router-dom';
 
 function Assistindo () {
-  const [atracao, setAtracao] = React.useState<Atracao | null>(null);
-
 
   return (
     <>
-      <h1>Thiago's Movie Database</h1>
-      <Link to="/" className='link-button'>Home</Link>        
-      <Link to="/filmes" className='link-button'>Filmes</Link>
-      <Link to="/seriados" className='link-button'>Séries</Link>
-      <Link to="/standup" className='link-button'>Stand-up</Link>
-      <Link to="/assistindo" className='link-button'>Assistindo</Link>
+            <div className="header-container">
+              <div className='titulo-principal'>
+                <h1 className='nome neon'>Thiago's Movie Database</h1>
+                <p>Sejam bem-vindos a minha lista de filmes, séries e mais.</p>
+              </div>
+              <div className='navbar'>
+                <Link to="/" className='link-button'>Home</Link>
+                <Link to="/filmes" className='link-button'>Filmes</Link>
+                <Link to="/seriados" className='link-button'>Séries</Link>
+                <Link to="/assistindo" className='link-button'>Assistindo</Link>
+              </div>
+            </div>
     </>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,36 +35,53 @@ function Home() {
 
   return (
     <>
-      <h1 className='nome neon'>Thiago's Movie Database</h1>
-      <p>Sejam bem-vindos a minha lista de filmes, séries e mais.</p>
-
-      <Link to="/" className='link-button'>Home</Link>
-      <Link to="/filmes" className='link-button'>Filmes</Link>
-      <Link to="/seriados" className='link-button'>Séries</Link>
-      <Link to="/assistindo" className='link-button'>Assistindo</Link>
+      <div className="header-container">
+        <div className='titulo-principal'>
+          <h1 className='nome neon'>Thiago's Movie Database</h1>
+          <p>Sejam bem-vindos a minha lista de filmes, séries e mais.</p>
+        </div>
+        <div className='navbar'>
+          <Link to="/" className='link-button'>Home</Link>
+          <Link to="/filmes" className='link-button'>Filmes</Link>
+          <Link to="/seriados" className='link-button'>Séries</Link>
+          <Link to="/assistindo" className='link-button'>Assistindo</Link>
+        </div>
+      </div>
 
       <div className="sugestao-container">
-        {carregando ? 
-          (<div>
-              <p></p>
-              <h3 className="neon">Carregando...</h3>
-              <div className="poster-container">
-                <img className="neon-border poster-vazio" src={posterVazio} alt="Poster vazio" />
-              </div>
-              <p className="plot"></p>
-           </div>)
-         : (atracao && 
-            (<div>
+      {carregando ? (
+        /* 
+        <>
+          <div className="poster-container">
+            <img className="neon-border poster-vazio" src={posterVazio} alt="Poster vazio" />
+          </div>
+          <div className="atracao-info-container">
+            <p> </p>
+            <h3 className="neon">Carregando...</h3>
+            <p className="plot"> </p>
+          </div>
+        </>
+        */ 
+        <>
+          <div className="poster-container">
+            <h3 className="neon">Carregando...</h3>
+          </div>
+        </>
+      ) : (
+        atracao && (
+          <>
+            <div className="poster-container">
+              <img className="neon-border poster" src={atracao.poster} alt={atracao.title} />
+            </div>
+            <div className="atracao-info-container">
               <p>{atracao.type}</p>
               <h3 className="neon">{atracao.title} ({atracao.year})</h3>
-              <div className="poster-container">
-                <img className="neon-border poster" src={atracao.poster} alt={atracao.title} />
-              </div>
               <p className="plot">{atracao.plot}</p>
-            </div>)
-            )
-        }
-      </div>
+            </div>
+          </>
+        )
+      )}
+    </div>
 
 
     </>
